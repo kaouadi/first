26d2ce151c8449ed37a9c61d0838bc3cddabd86a
Karim Aouadi <kdolphinus@gmail.com>
2011-08-16 18:45:36 +0200

    Refactored tests for controller Pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,9 +15,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-
-      response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -25,9 +27,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-
-      response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
@@ -39,9 +39,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-
-      response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 end
